2db100fc9543ac6c102150f204b60cb3ff00ecab
vienTM <thanminhvien@gmail.com>
2018-10-10 12:10:20 +0700

    add new Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreativeComponent } from './components/creative/creative.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreativeComponent
   ],
   imports: [
     BrowserModule
